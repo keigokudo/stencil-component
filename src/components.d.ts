321821552a779f7bb32d0844b04b6ebca500b30e
keigokudo <keigokudo55@googlemail.com>
2021-05-18 11:28:44 +0200

    generate stecil-button

--- src/components.d.ts
+++ src/components.d.ts
@@ -20,6 +20,8 @@ export namespace Components {
          */
         "middle": string;
     }
+    interface StencilButton {
+    }
 }
 declare global {
     interface HTMLMyComponentElement extends Components.MyComponent, HTMLStencilElement {
@@ -28,8 +30,15 @@ declare global {
         prototype: HTMLMyComponentElement;
         new (): HTMLMyComponentElement;
     };
+    interface HTMLStencilButtonElement extends Components.StencilButton, HTMLStencilElement {
+    }
+    var HTMLStencilButtonElement: {
+        prototype: HTMLStencilButtonElement;
+        new (): HTMLStencilButtonElement;
+    };
     interface HTMLElementTagNameMap {
         "my-component": HTMLMyComponentElement;
+        "stencil-button": HTMLStencilButtonElement;
     }
 }
 declare namespace LocalJSX {
@@ -47,8 +56,11 @@ declare namespace LocalJSX {
          */
         "middle"?: string;
     }
+    interface StencilButton {
+    }
     interface IntrinsicElements {
         "my-component": MyComponent;
+        "stencil-button": StencilButton;
     }
 }
 export { LocalJSX as JSX };
@@ -56,6 +68,7 @@ declare module "@stencil/core" {
     export namespace JSX {
         interface IntrinsicElements {
             "my-component": LocalJSX.MyComponent & JSXBase.HTMLAttributes<HTMLMyComponentElement>;
+            "stencil-button": LocalJSX.StencilButton & JSXBase.HTMLAttributes<HTMLStencilButtonElement>;
         }
     }
 }
