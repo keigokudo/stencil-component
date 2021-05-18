e04df0a768a74fa41ec42772cf4004404f542857
keigokudo <keigokudo55@googlemail.com>
2021-05-18 11:00:27 +0200

    add global style and applied it

--- stencil.config.ts
+++ stencil.config.ts
@@ -2,6 +2,7 @@ import { Config } from '@stencil/core';
 
 export const config: Config = {
   namespace: 'stencil-component',
+  globalStyle: 'src/global/global.css',
   outputTargets: [
     {
       type: 'dist',
